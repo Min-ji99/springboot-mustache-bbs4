fca43906c5b7be9c75b9eb96fb6d29c80863ff0d
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-14 11:19:19 +0900

    Add : product 테이블 Entity
    1. Product Table Entity
    2. application.yml ddl-auto update로

---
+++ src/main/java/com/mustache/bbs4/domain/entity/Product.java
@@ -0,0 +1,24 @@
+package com.mustache.bbs4.domain.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}

83ba2857108cce766002b875577185286ef1ab5f
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-14 14:53:12 +0900

    Add : find 쿼리 추가(LIKE, IN, BETWEEN)
    1. BusinessTypeName에 보건소, 보건지소, 보건진료소가 있는 레코드 검색
    2. RoadNameAddress에 강남구가 포함된 레코드 검색
    3. HospitalName이 연세로 시작하는 레코드 검색
    4. ToalNumberOfBeds가 10이상 20미만인 레코드 검색

--- src/main/java/com/mustache/bbs4/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs4/repository/HospitalRepository.java
@@ -3,5 +3,12 @@ package com.mustache.bbs4.repository;
 import com.mustache.bbs4.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+    List<Hospital> findByRoadNameAddressContaining(String address);
+
+    List<Hospital> findByHospitalNameStartingWith(String keyword);
+    List<Hospital> findByTotalNumberOfBedsBetween(int start, int end);
 }
---
+++ src/test/java/com/mustache/bbs4/repository/HospitalRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.mustache.bbs4.repository;
+
+import com.mustache.bbs4.domain.entity.Hospital;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HospitalRepositoryTest {
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    List<String> inClues;
+    @BeforeEach
+    void setup(){
+        inClues=new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+        inClues.add("보건진료소");
+    }
+    @Test
+    void name(){
+        Optional<Hospital> opt=hospitalRepository.findById(1);
+        Hospital hp=opt.get();
+        assertEquals(1, hp.getId());
+    }
+
+    @Test
+    void findByBusinessTypeNameIn(){
+        List<Hospital> hospitals=hospitalRepository.findByBusinessTypeNameIn(inClues);
+        for(var hospital : hospitals){
+            System.out.println(hospital.getBusinessTypeName());
+        }
+    }
+
+    @Test
+    void findByRoadNameAddressContaining(){
+        List<Hospital> hospitals=hospitalRepository.findByRoadNameAddressContaining("강남구");
+        for(var hospital : hospitals){
+            System.out.printf("%s %s\n", hospital.getHospitalName(), hospital.getRoadNameAddress());
+        }
+    }
+    @Test
+    void findByHospitalNameStartingWith(){
+        List<Hospital> hospitals=hospitalRepository.findByHospitalNameStartingWith("연세");
+        for(var hospital : hospitals){
+            System.out.printf("%s %s\n", hospital.getHospitalName(), hospital.getRoadNameAddress());
+        }
+    }
+
+    @Test
+    void findByTotalNumberOfBedsBetween(){
+        List<Hospital> hospitals=hospitalRepository.findByTotalNumberOfBedsBetween(10, 19);
+        for(var hospital : hospitals){
+            System.out.printf("%s %d\n", hospital.getHospitalName(), hospital.getTotalNumberOfBeds());
+        }
+    }
+}

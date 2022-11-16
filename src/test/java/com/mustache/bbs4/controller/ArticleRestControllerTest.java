27b19377d8a343b98c74e2044f8c262bca007b31
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-16 15:07:18 +0900

    Fix : ArticleRestController add 테스트

--- src/test/java/com/mustache/bbs4/controller/ArticleRestControllerTest.java
+++ src/test/java/com/mustache/bbs4/controller/ArticleRestControllerTest.java
@@ -1,5 +1,10 @@
 package com.mustache.bbs4.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mustache.bbs4.domain.dto.ArticleAddRequest;
+import com.mustache.bbs4.domain.dto.ArticleAddResponse;
+import com.mustache.bbs4.domain.dto.ArticleDto;
 import com.mustache.bbs4.domain.dto.ArticleResponse;
 import com.mustache.bbs4.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
@@ -7,11 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,27 +32,44 @@ class ArticleRestControllerTest {
 
     @MockBean
     ArticleService articleService;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     @DisplayName("1개의 Json 형태로 잘 나오는지")
-    void JsonResponse() throws Exception{
-        ArticleResponse articleResponse=ArticleResponse.builder()
-                .id((long)1)
-                .title("룰루")
-                .content("랄라")
-                .build();
+    void findSingle() throws Exception{
+        long articleId=1l;
 
-        given(articleService.getArticle((long)1))
-                .willReturn(articleResponse);
-
-        long articleId=1;
+        given(articleService.getArticleById(articleId))
+                .willReturn(new ArticleDto(1l, "룰루", "랄라"));
 
         String url=String.format("/api/v1/articles/%d", articleId);
         mockMvc.perform(get(url))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.title").exists())
-                .andExpect(jsonPath("$.title").value("룰루"))
+                .andExpect(jsonPath("$.content").exists())
                 .andDo(print());
-        verify(articleService).getArticle(articleId);
+
+        verify(articleService).getArticleById(articleId);
+    }
+    @Test
+    @DisplayName("add 기능 테스트")
+    void add() throws Exception {
+        ArticleAddRequest dto=new ArticleAddRequest("제목입니다", "내용입니다.");
+
+        given(articleService.add(any()))
+                .willReturn(new ArticleAddResponse(1l, dto.getTitle(), dto.getContent()));
+
+        mockMvc.perform(post("/api/v1/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new ArticleAddRequest("제목입니다", "내용입니다."))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.content").exists())
+                .andDo(print());
+
+        verify(articleService).add(any());
     }
 }
